585248aba699da719342af744c791a8afa4ecc65
Piotr Rybinski <prybinski@Piotrs-MacBook-Pro.local>
2022-02-03 22:50:56 +0100

    Requirements change, base scraper added, discount adn dupes handled

---
+++ baseScraper.py
@@ -0,0 +1,66 @@
+import json 
+
+
+class BaseScraper:
+    """ BaseScraper """
+    
+    def __init__(self):
+        self
+
+
+    def characterEncoder(string):
+        """ Transform uft character
+
+            Args:
+            string: String.
+
+            Returns:
+                String
+
+        """
+        return string.encode('utf-8','ignore').decode("utf-8")
+
+
+    def cleanResponse(title, description, discount, price):
+        """ Clean response from duplicates prepare dictionary for json
+
+            Args:
+            string: title
+            string: description
+            string: discount
+            string: price
+
+            Returns:
+                dictionary
+
+        """
+
+        list = [title, description, discount, price]
+
+        return {'Option title': BaseScraper.__removeDupes(list, title), 
+                'Description': BaseScraper.__removeDupes(list, description), 
+                'Price': BaseScraper.__removeDupes(list, price), 
+                'Discount:': BaseScraper.__removeDupes(list, discount)}
+
+
+    def __removeDupes(list, item):
+        """ Remove duplicated occurences of subsrtring in string
+
+            Args:
+            list: list
+            string: item
+
+            Returns:
+                string
+
+        """
+        for element in list:
+            # Get index of duplicate
+            index = item.find(element)
+            # Remove substring if index is greater than 0
+            # This will remove substring from string not a full string from another string
+            if index > 0:
+                item = item.replace(str(element), '')
+        
+        return item
+
--- scraper.py
+++ scraper.py
@@ -3,9 +3,9 @@ import os
 # Tell syspath where to import modules from other folders in root direcotry
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 from env import EnvReader
+from baseScraper import BaseScraper
 
 from selenium import webdriver
-from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -15,7 +15,7 @@ import random
 import json 
 
 
-class ProductScraper:
+class ProductScraper(BaseScraper):
     """ ProductScraper """
     
     def __init__(self):
@@ -42,18 +42,27 @@ class ProductScraper:
         except NoSuchElementException:
             pass
 
+        # Create dictionary to keep response with key => val
         response = {}
         # Get products
         try:
             products =  driver.find_elements_by_class_name(EnvReader.getProductsByClass(self))
 
             for i, product in enumerate(products):
-                title = product.find_element_by_tag_name(EnvReader.getTitle(self)).text
-                description = product.find_element_by_class_name(EnvReader.getDescription(self)).text
-                price = product.find_element_by_class_name(EnvReader.getPrice(self)).text
+                title = ProductScraper.characterEncoder(product.find_element_by_tag_name(EnvReader.getTitle(self)).text)
+                description = ProductScraper.characterEncoder(product.find_element_by_class_name(EnvReader.getDescription(self)).text)
+
+                # Get discount
+                discount = ''
+                try:
+                    discount = product.find_element_by_css_selector(EnvReader.getDiscount(self)).text
+                except:
+                    pass
+
+                price = ProductScraper.characterEncoder(product.find_element_by_class_name(EnvReader.getPrice(self)).text)
 
                 # Add elements to response dict
-                response[i] = {'Option title': title, 'Description': description, 'Price': price}
+                response[i] = ProductScraper.cleanResponse(title, description, discount, price)
 
         except NoSuchElementException:
             driver.close()
